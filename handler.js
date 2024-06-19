ca93355f5c163158ba826095a2651cd3b95cdd7a
saruu <saralarasaily100@gmail.com>
2024-06-19 17:42:39 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 exports.randomNumberGenerator = async (event) => {
  const randomNumber = parseInt(Math.random *10);
+ console.log(process.env.SERVERLESS_ACCESS_KEY)
  console.log("The random number is ", randomNumber);
  return randomNumber;
 };
