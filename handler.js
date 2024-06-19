4c58debb31a0d4d7523fa0acd673247224c2cdb2
saruu <saralarasaily100@gmail.com>
2024-06-19 17:43:27 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 exports.randomNumberGenerator = async (event) => {
  const randomNumber = parseInt(Math.random *10);
- console.log(process.env.SERVERLESS_ACCESS_KEY)
+ console.log(process.env.AWS_SECRET_ACCESS__KEY)
  console.log("The random number is ", randomNumber);
  return randomNumber;
 };
